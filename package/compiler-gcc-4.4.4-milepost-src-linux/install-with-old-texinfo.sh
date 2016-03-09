562428dd93e0da5cab52d6ae2759544e293b3c67
Grigori Fursin <Grigori.Fursin@cTuning.org>
2016-03-09 20:29:59 +0100

    removing outdated file

--- package/compiler-gcc-4.4.4-milepost-src-linux/install-with-old-texinfo.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-FGG experienced problem compiling this package with newer texinfo version >= 5.0
-
-Hence set path to old texinfo here before installing this package:
-export PATH=~/fggprogs/texinfo-4.13/bin:$PATH
-
-ck install package
